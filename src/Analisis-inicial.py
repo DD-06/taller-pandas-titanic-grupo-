80b40c21c4a5fa563bdf92be35515c400f8384db
Diego Daza <diegojosedaza@gmail.com>
2025-11-11 21:13:11 -0500

    Corrección del primer script

--- src/Analisis-inicial.py
+++ src/Analisis-inicial.py
@@ -4,8 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Cargar los datos desde la carpeta del proyecto
-test = pd.read_excel("data/test.xlsx")
-train = pd.read_excel("data/train.xlsx")
+test = pd.read_excel("data/test.csv.xlsx")
+train = pd.read_excel("data/train.csv.xlsx")
 
 print("Datos de entrenamiento:")
 print(train.head())
