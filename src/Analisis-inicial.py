3956ecab7b6976800258de244e34cffdd4779291
Diego Daza <diegojosedaza@gmail.com>
2025-11-11 21:11:50 -0500

    Primer Script

---
+++ src/Analisis-inicial.py
@@ -0,0 +1,24 @@
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargar los datos desde la carpeta del proyecto
+test = pd.read_excel("data/test.xlsx")
+train = pd.read_excel("data/train.xlsx")
+
+print("Datos de entrenamiento:")
+print(train.head())
+
+print("\nDatos de prueba:")
+print(test.head())
+
+print("\nInformación del conjunto de prueba:")
+print(test.info())
+
+print("\nInformación del conjunto de entrenamiento:")
+print(train.info())
+
+print("\nEstadísticas básicas de cada DataFrame:")
+print(train.describe())
+print(test.describe())
